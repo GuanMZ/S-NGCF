716573d00650256a62b904fff8be99877855c2a9
GuanMZ <guanmz14@mails.tsinghua.edu.cn>
2019-08-30 15:31:11 +0900

    Add files via upload

---
+++ NGCF/utility/parser.py
@@ -0,0 +1,87 @@
+'''
+Created on Oct 10, 2018
+Tensorflow Implementation of Neural Graph Collaborative Filtering (NGCF) model in:
+Wang Xiang et al. Neural Graph Collaborative Filtering. In SIGIR 2019.
+
+@author: Xiang Wang (xiangwang@u.nus.edu)
+'''
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run NGCF.")
+    parser.add_argument('--weights_path', nargs='?', default='',
+                        help='Store model path.')
+    parser.add_argument('--data_path', nargs='?', default='../Data/',
+                        help='Input data path.')
+    parser.add_argument('--proj_path', nargs='?', default='',
+                        help='Project path.')
+
+    parser.add_argument('--dataset', nargs='?', default='gowalla',
+                        help='Choose a dataset from {gowalla, yelp2018, amazon-book}')
+    parser.add_argument('--pretrain', type=int, default=0,
+                        help='0: No pretrain, -1: Pretrain with the learned embeddings, 1:Pretrain with stored models.')
+    parser.add_argument('--verbose', type=int, default=1,
+                        help='Interval of evaluation.')
+    parser.add_argument('--epoch', type=int, default=500,
+                        help='Number of epoch.')
+
+    parser.add_argument('--embed_size', type=int, default=64,
+                        help='Embedding size.')
+    parser.add_argument('--layer_size', nargs='?', default='[64]',
+                        help='Output sizes of every layer')
+    parser.add_argument('--batch_size', type=int, default=1024,
+                        help='Batch size.')
+
+    parser.add_argument('--regs', nargs='?', default='[1e-5,1e-5,1e-2]',
+                        help='Regularizations.')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='Learning rate.')
+
+    parser.add_argument('--model_type', nargs='?', default='ngcf',
+                        help='Specify the name of model (ngcf).')
+    parser.add_argument('--adj_type', nargs='?', default='norm',
+                        help='Specify the type of the adjacency (laplacian) matrix from {plain, norm, mean}.')
+    parser.add_argument('--alg_type', nargs='?', default='ngcf',
+                        help='Specify the type of the graph convolutional layer from {ngcf, gcn, gcmc}.')
+
+    parser.add_argument('--gpu_id', type=int, default=0,
+                        help='0 for NAIS_prod, 1 for NAIS_concat')
+
+    parser.add_argument('--node_dropout_flag', type=int, default=0,
+                        help='0: Disable node dropout, 1: Activate node dropout')
+    parser.add_argument('--node_dropout', nargs='?', default='[0.1]',
+                        help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
+    parser.add_argument('--mess_dropout', nargs='?', default='[0.1]',
+                        help='Keep probability w.r.t. message dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
+
+    parser.add_argument('--Ks', nargs='?', default='[20, 40, 60, 80, 100]',
+                        help='Evaluate@Ks')
+
+    parser.add_argument('--save_flag', type=int, default=0,
+                        help='0: Disable model saver, 1: Activate model saver')
+
+    parser.add_argument('--test_flag', nargs='?', default='part',
+                        help='Specify the test type from {part, full}, indicating whether the reference is done in mini-batch')
+
+    parser.add_argument('--report', type=int, default=0,
+                        help='0: Disable performance report w.r.t. sparsity levels, 1: Show performance report w.r.t. sparsity levels')    
+    
+    
+    # add agrs for Social-NGCF
+    parser.add_argument('--social', type=int, default=0,
+                        help='0: Without social connection, 1: With social connection')
+    parser.add_argument('--num_neg', type=int, default=4,
+                        help='Num of negative samples')
+    parser.add_argument('--report_loop', type=int, default=10,
+                        help='Report each report_loop number of epochs')
+    parser.add_argument('--shuffle_rate', type=float, default=0.,
+                        help='Ratio of random shuffle on social graph, from 0. to 1.')
+    parser.add_argument('--save_print_flag', type=int, default=0,
+                        help='0: Disable performance saver, 1: Activate performance saver')
+    
+    # add args for NMF.py
+    parser.add_argument('--keep_prob', nargs='?', default='0.5',
+                        help='NMF.py line 311: model.dropout_keep: eval(args.keep_prob),')
+    
+    
+    return parser.parse_args()
